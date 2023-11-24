39d80a693032cb3e1bbd2874df359ba37f23f817
Quantinx <josephtassell@gmail.com>
2023-11-24 16:09:01 +0100

    improved variables for darkmode

--- script.js
+++ script.js
@@ -27,14 +27,17 @@ hamburgerToggleEl.addEventListener("click", function (e) {
 
 function darkMode() {
   if (darkToggle === true) {
-    rootEl.style.setProperty("--default-menu-color", "grey");
-    rootEl.style.setProperty("--default-txt-color", "white");
-    rootEl.style.setProperty("--default-bg-color", "black");
+    rootEl.style.setProperty("--default-menu-color", "var( --dark-menu-color)");
+    rootEl.style.setProperty("--default-txt-color", "var(--dark-txt-color)");
+    rootEl.style.setProperty("--default-bg-color", "var( --dark-bg-color)");
     darkToggleEl.innerText = "Light Mode";
   } else {
-    rootEl.style.setProperty("--default-menu-color", "blue");
-    rootEl.style.setProperty("--default-txt-color", "black");
-    rootEl.style.setProperty("--default-bg-color", "white");
+    rootEl.style.setProperty(
+      "--default-menu-color",
+      "var( --light-menu-color)"
+    );
+    rootEl.style.setProperty("--default-txt-color", "var(--light-txt-color)");
+    rootEl.style.setProperty("--default-bg-color", "var( --light-bg-color)");
     darkToggleEl.innerText = "Dark Mode";
   }
 }
